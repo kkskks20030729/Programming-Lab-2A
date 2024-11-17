fe1cd48bc9925015040f90350524a828b8b566f1
kkskks20030729 <kkskks20030729@gmail.com>
2024-11-17 10:31:30 +0800

    Create main.hpp

---
+++ main.hpp
@@ -0,0 +1,4 @@
+//functions declaration
+double checkdiscriminant(int a, int b, int c);
+double getplus(double discriminant, int a, int b, int c);
+double getminus(double discriminant, int a, int b, int c);
