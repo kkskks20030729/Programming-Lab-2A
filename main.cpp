948af0eb1805f93cdef2b40c40e2ed5aba24daa2
kkskks20030729 <kkskks20030729@gmail.com>
2024-11-18 14:25:14 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-/*Lab2B*/
+/*Lab2C*/
 /*Write codes description here*/
-/*Name: Kua Khai Siang*/
-/*USM Email: kuakhaisiang2003@student.usm.my*/
-/*GitHub Username: kuakhaisiang2003*/
-/*Matric No.: 23300856*/
+/*Name: Write your full-name here*/
+/*USM Email: abc123@student.usm.my*/
+/*GitHub Username: abc123*/
+/*Matric No.: 987654*/
 
 #if __linux__                                           //DO NOT EDIT OR REMOVE
     #define CATCH_CONFIG_RUNNER                         //DO NOT EDIT OR REMOVE
@@ -14,8 +14,7 @@
 
 #include "main.hpp"                                     //Use this header file for all the functions' declaration
 #include <iostream>
-#include <cmath>
-#include <iomanip>
+#include <string>
 using namespace std;
 //Add any required header file(s)
 
@@ -25,42 +24,53 @@ int main(int argc, char* const argv[]){                 //DO NOT EDIT OR REMOVE
     #endif                                              //DO NOT EDIT OR REMOVE
     //add and complete your codes after this line
     
-    double r, theta, phi;
+    int mark;
     
-    std::cout << "Enter the spherical coordinates (r, theta, phi):\n";
-    std::cout << "Enter r:";
-    std::cin >> r;
-    std::cout << "\nEnter theta:";
-    std::cin >> theta;
-    std::cout << "\nEnter phi:";
-    std::cin >> phi;
-
-    std::cout << "Spherical coordinate entered: (" << r << ", " << theta << ", " << phi << ")\n";
+    cout << "Enter your mark > ";
+    cin >> mark;
     
-    double x = get_Px(r, theta, phi);
-    double y = get_Py(r, theta, phi);
-    double z = get_Pz(r, phi);
+    if (mark < 0 || mark > 100) {
+        cout << "Invalid mark. Re-run the program with mark between 0 and 100.";
+    } else {
     
-    std::cout << "Equivalent Cartesian coordinate: ("
-              << std::fixed << std::setprecision(3) << x << ", "
-              << std::fixed << std::setprecision(3) << y << ", "
-              << std::fixed << std::setprecision(3) << z << ")\n";
-
-return 0;
+        string grade = get_grade(mark);
+        
+        if (grade == "F" || grade == "C-" || grade == "D-" || grade == "D" || grade == "D+") {
+            cout << "Your grade: " << grade << " (FAIL)" << endl;
+        } else {
+            cout << "Your grade: " << grade << " (PASS)" << endl;
+        }
+    }
+    
+    return 0;
 }
 
-
-double get_Px(double r, double theta, double phi) {
-    return r * sin(phi) * cos(theta);
+string get_grade(int mark) {
+    if (mark >= 80 && mark <= 100) {
+        return "A";
+    } else if (mark >= 70 && mark <= 79) {
+        return "A-";
+    } else if (mark >= 64 && mark <= 69) {
+        return "B+";
+    } else if (mark >= 58 && mark <= 63) {
+        return "B";
+    } else if (mark >= 52 && mark <= 57) {
+        return "B-";
+    } else if (mark >= 46 && mark <= 51) {
+        return "C+";
+    } else if (mark >= 40 && mark <= 45) {
+        return "C";
+    } else if (mark >= 36 && mark <= 39) {
+        return "C-";
+    } else if (mark >= 32 && mark <= 35) {
+        return "D+";
+    } else if (mark >= 28 && mark <= 31) {
+        return "D";
+    } else if (mark >= 25 && mark <= 27) {
+        return "D-";
+    } else if (mark >= 0 && mark <= 24) {
+        return "F";
+    } else {
+        return "Invalid";
+    }
 }
-
-
-double get_Py(double r, double theta, double phi) {
-    return r * sin(phi) * sin(theta);
-}
-
-
-double get_Pz(double r, double phi) {
-    return r * cos(phi);
-}
-
